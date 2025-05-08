4d3e8f25c3c93cf860a934e115f9da48a5b4702e
jonathan <jonathanpressler@icloud.com>
2025-05-08 15:55:17 -0400

    Update click logging to differentiate between left and right quarters of the video

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -111,10 +111,10 @@ document.addEventListener("DOMContentLoaded", async function() {
         setTimeout(() => {
             overlayContext.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height); // Clear the overlay canvas
         }, 5000); // Hide after 2 seconds
-        if (eventX < videoWidth / 3) {
-            console.log("Left third clicked");
-        } else if (eventX > (videoWidth * 2) / 3) {
-            console.log("Right third clicked");
+        if (eventX < videoWidth / 4) {
+            console.log("Left quarter clicked");
+        } else if (eventX > (videoWidth * 2) / 4) {
+            console.log("Right quarter clicked");
         }
         isPaused = !isPaused;
         if (!isPaused) spinner();
