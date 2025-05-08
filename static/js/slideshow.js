77fb16bafd5a421c66e176dde5833b9e04501d62
jonathan <jonathanpressler@icloud.com>
2025-05-08 15:42:47 -0400

    Remove overlay canvas auto-clear functionality to maintain click coordinate visibility

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -112,9 +112,9 @@ document.addEventListener("DOMContentLoaded", async function() {
         overlayContext.fillText(`X: ${clickX.toFixed(2)}%, Y: ${clickY.toFixed(2)}%`, eventX + 15, eventY - 15);
         overlayContext.strokeStyle = "red";
         overlayContext.strokeRect(eventX - 10, eventY - 10, 20, 20); // Draw a border around the square
-        setTimeout(() => {
-            overlayContext.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height); // Clear the overlay canvas
-        }, 2000); // Hide after 2 seconds
+        // setTimeout(() => {
+        //     overlayContext.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height); // Clear the overlay canvas
+        // }, 2000); // Hide after 2 seconds
         isPaused = !isPaused;
         if (!isPaused) spinner();
         startSlideshow(streams);
